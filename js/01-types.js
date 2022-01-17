f783a98b1b71c1d9529fe57127544f0ff031dacb
unknown <bhm727@yahoo.com>
2022-01-17 12:42:05 -0500

    Types Demo Code

--- js/01-types.js
+++ js/01-types.js
@@ -1,61 +1,47 @@
-let username: string = "instructor";
+var username = "instructor";
 // let password: string = "Letmein1234!"; // Cannot redeclare password twice as you have done below
-let instructorId: number = 5;
+var instructorId = 5;
 // Or
-let password: string;
-let phoneNumber: number;
-let email: string;
-let socialSecurityNumber: number;
-let isMarried: boolean;
+var password;
+var phoneNumber;
+var email;
+var socialSecurityNumber;
+var isMarried;
 //And
-let greeting = "Hello";
-
+var greeting = "Hello";
 //Compile-Time Error ex.
 // let password: string;
 // password = 1234; is not string. is number
-
 //Number
-
-let age: number = 40;
-let currentYear: number = 2018
+var age = 40;
+var currentYear = 2018;
 //TypeScript supports ES6 octal literals
-let decimal: number = 6;
-let hex: number = 0xf00d;
-let binary: number = 0b1010;
-
+var decimal = 6;
+var hex = 0xf00d;
+var binary = 10;
 //String
-
-let companyName: string;
-companyName = "Eleven Fifty"
+var companyName;
+companyName = "Eleven Fifty";
 //can use different quotation types '' and "" in the same method
-
 //Boolean
-
-let isLoggedIn: boolean = true;
-let isGoldLevelUser: boolean = true;
-
+var isLoggedIn = true;
+var isGoldLevelUser = true;
 //Array - can be expressed in 2 different ways
-
 //1
-let studentList: string[] = ['Tom', 'Tracy', 'Nikaya']
+var studentList = ['Tom', 'Tracy', 'Nikaya'];
 //2
-let allScores: Array<number> = [100, 92, 95, 96]
-
+var allScores = [100, 92, 95, 96];
 //Accessing the array indices works in the same way
 console.log("Student List:", studentList[0]);
-
-let dataFromThirdParty: any = '12345';
+var dataFromThirdParty = '12345';
 console.log(dataFromThirdParty);
 console.log(typeof dataFromThirdParty);
-
 //Any Type (typeof)
 dataFromThirdParty = 12345;
 console.log(dataFromThirdParty);
 console.log(typeof dataFromThirdParty);
-
 // VOID - seen in functions that return no value
-
-function sayHelloToAll() : void {
-    console.log("Hello to all!")
+function sayHelloToAll() {
+    console.log("Hello to all!");
 }
-
+//# sourceMappingURL=01-types.js.map
